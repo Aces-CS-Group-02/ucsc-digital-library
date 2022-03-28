ba41c576b9c36525acdaa529414b1cecb6287ece
= <kasunjalitha77@gmail.com>
2022-03-28 09:44:38 +0530

    pre-reabse commit

--- models/ContentSubmissionStatus.php
+++ models/ContentSubmissionStatus.php
@@ -2,9 +2,12 @@
 
 namespace app\models;
 
+use app\core\Application;
 use app\core\DbModel;
+use Exception;
 
-class ContentSubmissionStatus extends DbModel{
+class ContentSubmissionStatus extends DbModel
+{
     public int $id = 0;
     public int $content_id = 0;
     public bool $is_approved = false;
@@ -36,4 +39,39 @@ class ContentSubmissionStatus extends DbModel{
     {
         return parent::save();
     }
+
+    // public function approve($contentId, $reason)
+    // {
+    //     $notificationModel = new Notification();
+    //     $notificationModel->loadData(['msg' => "Your submission is aprroved by library staff"]);
+    //     if (!$notificationModel->validate()) return false;
+    //     $notificationReceiverModel = new NotificationReceiver();
+
+
+    //     $where = [
+    //         "content_id" => $contentId
+    //     ];
+    //     $contents = new Content;
+    //     $content = $contents->findOne($where);
+
+    //     if ($content) {
+    //         $contentSubmissionStatus = new ContentSubmissionStatus();
+    //         $contentSubmissionStatus->content_id = $content->content_id;
+    //         $contentSubmissionStatus->is_approved = true;
+    //         $contentSubmissionStatus->reason = $reason;
+    //         $contentSubmissionStatus->approved_by = Application::$app->user->reg_no;
+
+    //         try {
+    //             Application::$app->db->pdo->beginTransaction();
+    //             $this->save();
+    //             $contents->UpdateApprovedState($content->content_id);
+    //             $notificationReceiverModel->setMultipleReceviers(Application::$app->db->pdo->lastInsertId(), $staff_members_list);
+    //             Application::$app->db->pdo->commit();
+    //             return true;
+    //         } catch (Exception $e) {
+    //             Application::$app->db->pdo->rollBack();
+    //             return false;
+    //         }
+    //     }
+    // }
 }
